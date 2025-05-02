fb9a1379f3c73e856330d6d4f0a8f57130b73135
Javiera Vega <javieravegareyes7@gmail.com>
2025-05-02 20:35:37 +0000

    creation of to do list with react

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const [task, setTask] = useState({
+		task: "",
+		isDone: false
+	})
+
+	const [listtask, setListTask] = useState([])
+
+
+	const handleChange = (event) => {
+		setTask({
+			...task,
+			task: event.target.value,
+		})
+	}
+
+	const addTask = (event) => {
+
+		if (event.key == "Enter") {
+			if (task.task.trim() == "") return
+			setListTask([
+				...listtask,
+				task
+			])
+			setError(false)
+
+			setTask({
+				task: "",
+				isDone: false
+			})
+		}
+	}
+
+	const deleteTask = (taskDelete) => {
+		const newListTask = listtask.filter((element, index) => index != taskDelete)
+		setListTask(newListTask)
+	}
+
+
+return (
+	<div className="container">
+		<div className="row justify-content-center">
+			<h1 className="title text-center">todos</h1>
+			<div className="col-6 border border-seconday bg-white box">
+				<form className="list-task"
+					onSubmit={(event) => event.preventDefault()}>
+
+					<input
+						className="form-task col-12"
+						type="text"
+						placeholder="ingresa tu tarea aquí"
+						name="taskname"
+						onKeyDown={addTask}
+						onChange={handleChange}
+						value={task.taskname}
+					/>
+				</form>
+				<ul>
+					{
+						listtask.map((element, index) => {
+							return (
+								<li
+									className="added-tasks"
+									key={index}>{element.task}
+									<span>
+										<i className="click fa-solid fa-xmark"
+											onClick={() => deleteTask(index)}
+										></i>
+									</span>
+								</li>
+							)
+						})
+					}
+				</ul>
+				<p className="text-body text-muted">{`${listtask.length} item left`}
+				</p>
+			</div>
+
 		</div>
-	);
+	</div>
+	
+
+)
 };
 
 export default Home;
